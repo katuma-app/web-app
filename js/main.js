ec3a24f771a7f5522ebae2768a20718bc807bd0d
adrian_ortuzar <adrianortuzar@gmail.com>
2013-11-16 00:27:00 +0100

    Set up requirejs in main.js and index.html file, loading all the dependencies through requirejs

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
+"use strict";
 
+require.config({
+    urlArgs: "nocache=" + (new Date()).getTime(), // In order to expire
+    baseUrl: "js/libs",
+    paths : {
+        backbone : "backbone",
+        underscore : "underscore",
+        jquery : "jquery-1.10.1",
+        marionette : "backbone.marionette.min",
+        "backbone.wreqr" : "backbone.wreqr",
+        "backbone.eventbinder" : "backbone.eventbinder",
+        "backbone.babysitter" : "backbone.babysitter"
+    },
+    shim : {
+        jquery : {
+            exports : "jQuery",
+        },
+        underscore : {
+            exports : "_"
+        },
+        backbone : {
+            deps : ["jquery", "underscore"],
+            exports : "Backbone",
+        },
+        marionette:{
+            deps:["backbone"]
+        },
+    }
+});
+
+define(function (require) {
+    // dependencies
+    var Backbone = require("Backbone");
+    require("marionette");
+
+    //start Katuma app
+    new Backbone.Marionette.Application();
+});
