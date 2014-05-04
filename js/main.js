2c0819ad0e8197932a6254d429fbf14e046c2c46
adrian_ortuzar <adrianortuzar@gmail.com>
2014-05-04 17:04:37 +0200

    Create bower.json to set the project libraries dependencies. Ignore libs folder. Set require modules to the news libraries paths.

--- js/main.js
+++ js/main.js
@@ -4,16 +4,15 @@ require.config({
     urlArgs: "nocache=" + (new Date()).getTime(), // In order to expire
     baseUrl: "js",
     paths : {
-        backbone : "libs/backbone",
-        underscore : "libs/underscore",
-        jquery : "libs/jquery-1.10.1",
-        marionette : "libs/backbone.marionette.min",
-        handlebars: "libs/handlebars-v1.1.2",
+        backbone : "libs/backbone/backbone",
+        underscore : "libs/underscore/underscore",
+        jquery : "libs/jquery/dist/jquery",
+        marionette : "libs/backbone.marionette/lib/backbone.marionette",
+        handlebars: "libs/handlebars/handlebars",
         templates: "templates",
-        "backbone.wreqr" : "libs/backbone.wreqr",
-        "backbone.eventbinder" : "libs/backbone.eventbinder",
-        "backbone.babysitter" : "libs/backbone.babysitter",
-        bootstrap:"libs/bootstrap.min"
+        "backbone.wreqr" : "libs/backbone.wreqr/lib/backbone.wreqr",
+        "backbone.babysitter" : "libs/backbone.babysitter/lib/backbone.babysitter",
+        bootstrap:"libs/bootstrap/dist/js/bootstrap"
     },
     shim : {
         jquery : {
